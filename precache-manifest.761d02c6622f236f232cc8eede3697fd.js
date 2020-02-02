f959052f11e5862cd5cc8bc72d2b232e197a9394
sharperprogrammer <sharperprogrammer@gmail.com>
2020-02-02 12:01:49 -0500

    Updates

---
+++ precache-manifest.761d02c6622f236f232cc8eede3697fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d57236b5643c9aec3467d01bb73cad84",
+    "url": "/react_pokedex/index.html"
+  },
+  {
+    "revision": "b1031b1f8353a1518abc",
+    "url": "/react_pokedex/static/css/main.c9cab6cc.chunk.css"
+  },
+  {
+    "revision": "20d8ba21ff79b9d4ddef",
+    "url": "/react_pokedex/static/js/2.2669dc3a.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react_pokedex/static/js/2.2669dc3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1031b1f8353a1518abc",
+    "url": "/react_pokedex/static/js/main.38652c45.chunk.js"
+  },
+  {
+    "revision": "8e936c5deba6966ac897",
+    "url": "/react_pokedex/static/js/runtime-main.c538c980.js"
+  }
+]);
