be653d989523916f26fffdb8e8b4f16b3882523f
sharperprogrammer <sharperprogrammer@gmail.com>
2020-02-02 16:43:16 -0500

    Use bigger images for each Pokemon.

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -5,7 +5,8 @@ export const Card = (props) => (
 
     <div className="card-container">
         {/* <img alt="monster" src={`${props.monster.sprite}`}></img> */}
-        <img alt="monster" src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${props.monster.id}.png`}></img>
+        {/* <img alt="monster" src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${props.monster.id}.png`}></img> */}
+        <img className="image" alt="monster" src={`https://img.pokemondb.net/artwork/large/${props.monster.name}.jpg`}></img>
         <h2 className="name"> {props.monster.name} </h2>
         
         { props.monster.type1 &&
