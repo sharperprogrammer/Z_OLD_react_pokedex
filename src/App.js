b3cb439189bcd19911213de234df1700ee78e8db
sharperprogrammer <sharperprogrammer@gmail.com>
2020-02-02 11:56:58 -0500

    Lists out the pokemon and their sprites.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import { CardList } from './components/card-list/card-list.component';
-import { SearchBox } from './components/search-box/search-box.component';
+// import { SearchBox } from './components/search-box/search-box.component';
 
 
 class App extends Component {
@@ -10,20 +10,51 @@ class App extends Component {
     super();
 
     this.state = {
-      all_pokemon: []
+      all_pokemon: [
+
+      ]
     };
   }
 
   componentDidMount() {
     fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
     .then(response => response.json())
-    .then(pokemon => this.setState({all_pokemon: pokemon.results}));
+    .then(pokemon => this.derp(pokemon));
+    
+  }
+
+  derp = (poke_results) => {
+    this.setState({all_pokemon: poke_results.results});
+    // this.state.all_pokemon.forEach(item => this.setProperties(item))
+  }
+
+  setProperties = (item) => {
+    this.setId(item);
+    this.setSprite(item);
+  }
+
+  setId = item => {
+    let id = item.url.substring(34, item.url.lastIndexOf("/"))
+    // console.log(id);
+    item.id = id;
+  }
+
+  setSprite = item => {
+    fetch(item.url)
+    .then(response => response.json())
+    .then(response => item.sprite = response.sprites.front_default)
   }
 
   render() {
+
+    // this.state.all_pokemon.forEach(item => console.log(item));
+
+    this.state.all_pokemon.forEach(item => this.setProperties(item))
+
     return (
       <div className="App">
         <h1>Pokedex</h1>
+        <CardList monsters={this.state.all_pokemon} />
       </div>
     );
   }
--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -2,9 +2,10 @@ import React from 'react'
 import './card.styles.css'
 
 export const Card = (props) => (
+
     <div className="card-container">
-        <img alt="monster" src={`https://robohash.org/${props.monster.id + 100}?set=set2`}></img>
+        {/* <img alt="monster" src={`${props.monster.sprite}`}></img> */}
+        <img alt="monster" src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${props.monster.id}.png`}></img>
         <h2> {props.monster.name} </h2>
-        <p> {props.monster.email} </p>
     </div>
 )
