9febd0838d228e138fdaa2cd0a24a73724ed2a80
sharperprogrammer <sharperprogrammer@gmail.com>
2020-02-03 10:04:50 -0500

    changed card-list component from a function to a Class

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import { CardList } from './components/card-list/card-list.component';
+import CardList from './components/card-list/card-list.component';
 import { SearchBox } from './components/search-box/search-box.component';
 // import update from 'react-addons-update';
 import update from 'immutability-helper';
--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
+import React, {Component} from 'react'
 import './card-list.styles.css'
 import { Card } from '../card/card.component';
 
-export const CardList = props => {
-    return (
-        <div className="card-list">
-            {props.monsters.map(monster => (
+class CardList extends Component {
+    render() {
+        return <div className="card-list">
+            {this.props.monsters.map(monster => (
                 <Card key={monster.id} monster={monster} />
             ))}
         </div>
-    );
+    };
 }
+
+export default CardList;
